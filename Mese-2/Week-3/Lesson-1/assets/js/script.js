23052fed2d38ea97d100da7f6fa8376c12164bb7
xAldox995 <valzanialdo@gmail.com>
2024-07-22 16:09:31 +0200

    Fine esercizio

--- Mese-2/Week-3/Lesson-1/assets/js/script.js
+++ Mese-2/Week-3/Lesson-1/assets/js/script.js
@@ -1,30 +1,90 @@
-console.log('Esercizio USER')
- class User {
-    constructor(_firstName, _lastName, _age, _location) {
-        this.firstName = _firstName;
-        this.lastName = _lastName;
-        this.age = _age;
-        this.location = _location;
+console.log("Esercizio USER");
+class User {
+  constructor(_firstName, _lastName, _age, _location) {
+    this.firstName = _firstName;
+    this.lastName = _lastName;
+    this.age = _age;
+    this.location = _location;
+  }
+  comparation = function (obj) {
+    if (this.age > obj.age) {
+      return obj.firstName + " è più giovane di " + this.firstName;
+    } else if (this.age < obj.age) {
+      return obj.firstName + " è più vecchio di " + this.firstName;
+    } else {
+      return obj.firstName + " è vecchio come " + this.firstName;
     }
-    comparation = function(obj){
-        if (this.age > obj.age){
-            return obj.firstName + ' è più giovane di ' + this.firstName;
-        }else if (this.age < obj.age) {
-            return obj.firstName + ' è più vecchio di ' + this.firstName;
-        } else {
-            return obj.firstName + ' è vecchio come ' + this.firstName;
-        }
+  };
+}
+
+const user1 = new User("Mario", "Rossi", 20, "Milano");
+const user2 = new User("Davide", "Bianchi", 25, "Napoli");
+const user3 = new User("Aldo", "Verdi", 30, "Bari");
+const user4 = new User("Giovanni", "Scem", 20, "Palermo");
+const user5 = new User("Dante", "Ferro", 25, "Firenze");
+const user6 = new User("Virgilio", "Impero", 30, "Roma");
+
+console.log(user1.comparation(user6));
+
+
+
+console.log('Esercizio PET'); 
+
+class Pet {
+  constructor(_petName, _ownerName, _species, _breed) {
+    this.petName = _petName;
+    this.ownerName = _ownerName;
+    this.species = _species;
+    this.breed = _breed;
+  }
+  sameOwner = function (obj) {
+    if (this.ownerName = obj.ownerName) {
+        return true
     }
- }
+  }
+}
 
- const user1 = new User ('Mario', 'Rossi', 20, 'Milano');
- const user2 = new User ('Davide', 'Bianchi', 25, 'Napoli');
- const user3 = new User ('Aldo', 'Verdi', 30, 'Bari');
- const user4 = new User ('Giovanni', 'Scem', 20, 'Palermo');
- const user5 = new User ('Dante', 'Ferro', 25, 'Firenze');
- const user6 = new User ('Virgilio', 'Impero', 30, 'Roma');
+const petList = [];
 
- console.log(user1.comparation(user6));
-
- 
-
+document.getElementsByTagName('form')[0].addEventListener('submit', function (e) {e.preventDefault();
+    const petNameValue = document.getElementById('petName').value;
+    const ownerNameValue = document.getElementById('ownerName').value;
+    const speciesValue = document.getElementById('species').value;
+    const breedValue = document.getElementById('breed').value;
+    
+    const element = new Pet(petNameValue, ownerNameValue, speciesValue, breedValue);
+    
+    petList.push(element);
+    
+    const unorderedList = document.getElementById('petList');
+    
+    unorderedList.innerHTML = '';
+    
+    for (let i = 0; i < petList.length; i++) {
+        // quest'operazione verrà ripetuta per OGNI elemento della memoria
+        // creo un <li> vuoto
+        const newLi = document.createElement('li') // <li>  </li>
+        // ora lo riempio con i dati dell'elemento singolo della memoria
+        newLi.innerText =
+          petList[i].petName +
+          ' ' +
+          petList[i].ownerName +
+          ' | ' +
+          petList[i].species +
+          ' ' +
+          petList[i].breed
+        // Stefano Casasola | 123123
+        // <li>Stefano Casasola | 123123</li>
+        newLi.classList.add('list-group-item')
+        // <li class="list-group-item">Stefano Casasola | 123123</li>
+        // ora, passo finale, appendo l'<li> appena creato e finito
+        // nella lista non ordinata
+        unorderedList.appendChild(newLi)
+      }
+    
+      // resetto il form
+      // firstNameInput.value = ''
+      // lastNameInput.value = ''
+      // phoneInput.value = ''
+      document.getElementsByTagName('form')[0].reset()
+}); 
