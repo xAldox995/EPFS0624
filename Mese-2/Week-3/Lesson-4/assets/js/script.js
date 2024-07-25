75b6b3bc851782924da0cee7a30a3f73d9cf08a2
xAldox995 <valzanialdo@gmail.com>
2024-07-25 16:37:58 +0200

    Errore su JS

--- Mese-2/Week-3/Lesson-4/assets/js/script.js
+++ Mese-2/Week-3/Lesson-4/assets/js/script.js
@@ -1,25 +1,25 @@
-
 const getImage = function (query) {
-    fetch('https://api.pexels.com/v1/search?query=' + query, {
-        headers: {
-            Authorizzation : 'hYEaBjSs4pF6PvZAQDn20vd8RRox2g1UV9vVi12uNloPydZQMVh4SE8r'
-        }
-    })
+  fetch("https://api.pexels.com/v1/search?query=" + query, {
+    headers: {
+      Authorizzation:
+        "hYEaBjSs4pF6PvZAQDn20vd8RRox2g1UV9vVi12uNloPydZQMVh4SE8r"
+    },
+  })
     .then((response) => {
-        console.log('Ecco la response', response)
-        if (response.ok) {
-            return response.json()
-        } else {
-            throw new Error ('Attenzione al response')
-        }
+      console.log("Ecco la response", response);
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Attenzione al response");
+      }
+    })
+    .then((data) => {
+      console.log("Ecco i dati", data);
     })
 
-        .catch((error) =>{
-            console.log('Attenzione',error)
-        })
-    }
-    
-  
+    .catch((error) => {
+      console.log("Attenzione", error);
+    });
+};
 
-
-getImage('crypto')
+getImage();
