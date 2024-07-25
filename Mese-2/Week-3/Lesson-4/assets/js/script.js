01f866263f7b8f5269142fa8cb0b00ca2ab54b12
xAldox995 <valzanialdo@gmail.com>
2024-07-25 17:46:49 +0200

    aggiusti

--- Mese-2/Week-3/Lesson-4/assets/js/script.js
+++ Mese-2/Week-3/Lesson-4/assets/js/script.js
@@ -13,46 +13,10 @@ const getImages = function (query) {
       }
     })
     .then((data) => {
-      console.log("Ecco i dati", data);
-      data.array.forEach((element) => {
-        const newCol = `            
-        <div class="col-md-4">
-              <div class="card mb-4 shadow-sm">
-                <img
-                  src=${element.url}
-                  class="bd-placeholder-img card-img-top"
-                />
-                <div class="card-body">
-                  <h5 class="card-title">Lorem Ipsum</h5>
-                  <p class="card-text">
-                    This is a wider card with supporting text below as a natural
-                    lead-in to additional content. This content is a little bit
-                    longer.
-                  </p>
-                  <div
-                    class="d-flex justify-content-between align-items-center"
-                  >
-                    <div class="btn-group">
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        View
-                      </button>
-                      <button
-                        type="button"
-                        class="btn btn-sm btn-outline-secondary"
-                      >
-                        Edit
-                      </button>
-                    </div>
-                    <small class="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>`;
-            const rowCards = document.getElementById('rowCards')
-            rowCards.innerHTML += newCol
+      console.log("Ecco i dati", data.photos);
+      let allTheDogImgs = document.querySelectorAll(".card img");
+      allTheDogImgs.forEach((dog, i) => {
+        dog.src = data.photos[i].src.medium;
       });
     })
 
@@ -61,4 +25,4 @@ const getImages = function (query) {
     });
 };
 
-getImages(crypto);
+// getImages('crypto');
