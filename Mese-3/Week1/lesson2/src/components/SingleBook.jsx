29e5984078041f3dbddae43996e97f7ae8d39a95
xAldox995 <valzanialdo@gmail.com>
2024-08-21 17:08:00 +0200

    finish the components of today

--- Mese-3/Week1/lesson2/src/components/SingleBook.jsx
+++ Mese-3/Week1/lesson2/src/components/SingleBook.jsx
@@ -3,39 +3,49 @@ import { Button, Card, Form } from "react-bootstrap";
 class SingleBook extends Component {
   state = {
     selected: {
-      chosen : false,
+      chosen: false,
     },
   };
+
+  toggleSelected = () => {
+    this.setState((prevState) => ({
+      selected: {
+        ...prevState.selected,
+        chosen: !prevState.selected.chosen,
+      },
+    }));
+  };
+
   render() {
+    const { book } = this.props;
+    const { chosen } = this.state.selected;
+
+    const cardStyle = chosen ? { border: "3px solid red" } : {};
+
+
     return (
-      <Card className="w-100 d-flex flex-column">
+      <Card className="w-100 d-flex flex-column" style={cardStyle}>
         <div className="flex-grow-1 d-flex">
           <Card.Img
             variant="top"
-            src={this.props.book.img}
+            src={book.img}
             className="img-fluid"
             style={{ height: "200px", objectFit: "cover", width: "100%" }}
+            onClick={this.toggleSelected}
           />
         </div>
         <Card.Body className="d-flex flex-column">
-          <Card.Title>{this.props.book.title}</Card.Title>
-          <Card.Text className="flex-grow-1">
-            {this.props.book.category}
-          </Card.Text>
+          <Card.Title>{book.title}</Card.Title>
+          <Card.Text className="flex-grow-1">{book.category}</Card.Text>
           <Button variant="primary" className="my-auto">
             More Info
           </Button>
           <Form.Group className="m-2" id="formGridCheckbox">
-            <Form.Check type="checkbox" label="SELECTED" 
-            onChange= {(e) => {
-                this.setState ({
-                    selected : {
-                        ...this.state.selected,
-                        chosen : e.target.checked,
-                    }
-                })
-            }}
-            checked = {this.state.selected.chosen}
+            <Form.Check
+              type="checkbox"
+              label="SELECTED"
+              onChange={this.toggleSelected}
+              checked={chosen}
             />
           </Form.Group>
         </Card.Body>
