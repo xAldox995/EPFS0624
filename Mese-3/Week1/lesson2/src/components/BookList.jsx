c25d53dd159722e0cd0d9350e8b0e084d6e5f6f3
xAldox995 <valzanialdo@gmail.com>
2024-08-21 16:47:49 +0200

    adds some components

--- Mese-3/Week1/lesson2/src/components/BookList.jsx
+++ Mese-3/Week1/lesson2/src/components/BookList.jsx
@@ -1,26 +1,54 @@
 import React, { Component } from "react";
-import {Row, Col } from "react-bootstrap";
+import { Row, Col, Form } from "react-bootstrap";
 import fantasy from "../data/fantasy.json";
 import SingleBook from "./SingleBook";
 
 class BookList extends Component {
-    render (){
-        return (
-            <Row xs={1} md={3} lg={6} className="g-4 mx-2">
-                {fantasy.map((book) => (
-                    <Col key={book.asin} className="d-flex">
-                    <SingleBook book = {book}/>
-                    </Col>
+  state = {
+    searchBook: {
+      name: "",
+    },
+  };
 
-                ))}
-            </Row>
-        )
-    }
-    }
-
-
-    //   <Row xs={1} md={3} lg={6} className="g-4 mx-2">
-    //   </Row>
-
+  getFilteredBooks() {
+    const {name}  = this.state.searchBook;
+    return fantasy.filter((book) =>
+      book.title.toLocaleLowerCase().includes(name.toLocaleLowerCase())
+    );
+  }
+  render() {
+    const filteredBooks = this.getFilteredBooks();
+    return (
+      <>
+        <Form className="mx-5 my-4">
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Cerca il tuo libro</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Il libro che stai cercando"
+              onChange={(e) => {
+                console.log("usa la tasiera scemo", e.target.value);
+                this.setState({
+                  searchBook: {
+                    ...this.state.searchBook,
+                    name: e.target.value,
+                  },
+                });
+              }}
+              value={this.state.searchBook.name}
+            />
+          </Form.Group>
+        </Form>
+        <Row xs={1} md={3} lg={6} className="g-4 mx-2">
+          {filteredBooks.map((book) => (
+            <Col key={book.asin} className="d-flex">
+              <SingleBook book={book} />
+            </Col>
+          ))}
+        </Row>
+      </>
+    );
+  }
+}
 
 export default BookList;
--- Mese-3/Week1/lesson2/src/components/SingleBook.jsx
+++ Mese-3/Week1/lesson2/src/components/SingleBook.jsx
@@ -1,29 +1,47 @@
-import React from "react";
-import { Button, Card } from "react-bootstrap";
-
-const SingleBook = function (props) {
-  return (
-    // <Col key={props.book.asin} className="d-flex">
+import React, { Component } from "react";
+import { Button, Card, Form } from "react-bootstrap";
+class SingleBook extends Component {
+  state = {
+    selected: {
+      chosen : false,
+    },
+  };
+  render() {
+    return (
       <Card className="w-100 d-flex flex-column">
         <div className="flex-grow-1 d-flex">
           <Card.Img
             variant="top"
-            src={props.book.img}
+            src={this.props.book.img}
             className="img-fluid"
             style={{ height: "200px", objectFit: "cover", width: "100%" }}
           />
         </div>
         <Card.Body className="d-flex flex-column">
-          <Card.Title>{props.book.title}</Card.Title>
-          <Card.Text className="flex-grow-1">{props.book.category}</Card.Text>
-          <Button variant="primary" className="mt-auto">
+          <Card.Title>{this.props.book.title}</Card.Title>
+          <Card.Text className="flex-grow-1">
+            {this.props.book.category}
+          </Card.Text>
+          <Button variant="primary" className="my-auto">
             More Info
           </Button>
+          <Form.Group className="m-2" id="formGridCheckbox">
+            <Form.Check type="checkbox" label="SELECTED" 
+            onChange= {(e) => {
+                this.setState ({
+                    selected : {
+                        ...this.state.selected,
+                        chosen : e.target.checked,
+                    }
+                })
+            }}
+            checked = {this.state.selected.chosen}
+            />
+          </Form.Group>
         </Card.Body>
       </Card>
-    // </Col>
-  );
-};
+    );
+  }
+}
 
-
-export default SingleBook
+export default SingleBook;
