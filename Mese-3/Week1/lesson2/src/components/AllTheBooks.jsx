241b137c8c5ebf610da756e5ad3e9dc375fab1ed
xAldox995 <valzanialdo@gmail.com>
2024-08-21 09:37:06 +0200

    coversion in class

--- Mese-3/Week1/lesson2/src/components/AllTheBooks.jsx
+++ Mese-3/Week1/lesson2/src/components/AllTheBooks.jsx
@@ -1,30 +1,34 @@
+import React, { Component } from "react";
 import { Button, Card, Row, Col } from "react-bootstrap";
 import fantasy from "../data/fantasy.json";
 
-const AllTheBooks = function () {
-  return (
-    <Row xs={1} md={3} lg={6} className="g-4 mx-2">
-      {fantasy.map((book) => (
-        <Col key={book.asin} className="d-flex">
-          <Card className="w-100 d-flex flex-column">
-            <div className="flex-grow-1 d-flex">
-              <Card.Img 
-                variant="top" 
-                src={book.img} 
-                className="img-fluid" 
-                style={{ height: '200px', objectFit: 'cover', width: '100%' }} 
-              />
-            </div>
-            <Card.Body className="d-flex flex-column">
-              <Card.Title>{book.title}</Card.Title>
-              <Card.Text className="flex-grow-1">{book.category}</Card.Text>
-              <Button variant="primary" className="mt-auto">More Info</Button>
-            </Card.Body>
-          </Card>
-        </Col>
-      ))}
-    </Row>
-  );
-};
+class AllTheBooks extends Component {
+  render() {
+    return (
+      <Row xs={1} md={3} lg={6} className="g-4 mx-2">
+        {fantasy.map((book) => (
+          <Col key={book.asin} className="d-flex">
+            <Card className="w-100 d-flex flex-column">
+              <div className="flex-grow-1 d-flex">
+                <Card.Img 
+                  variant="top" 
+                  src={book.img} 
+                  className="img-fluid" 
+                  style={{ height: '200px', objectFit: 'cover', width: '100%' }} 
+                />
+              </div>
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{book.title}</Card.Title>
+                <Card.Text className="flex-grow-1">{book.category}</Card.Text>
+                <Button variant="primary" className="mt-auto">More Info</Button>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    );
+  }
+}
 
 export default AllTheBooks;
+
