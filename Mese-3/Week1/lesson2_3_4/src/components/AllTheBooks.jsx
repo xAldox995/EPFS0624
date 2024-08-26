4ceb1b2dbd1fff4eb6856090eeb5f32bd2ef4279
xAldox995 <valzanialdo@gmail.com>
2024-08-26 16:28:21 +0200

    c

--- Mese-3/Week1/lesson2_3_4/src/components/AllTheBooks.jsx
+++ Mese-3/Week1/lesson2_3_4/src/components/AllTheBooks.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { Button, Card, Row, Col } from "react-bootstrap";
 import fantasy from "../data/fantasy.json";
+import SingleBook from "./SingleBook";
 
 class AllTheBooks extends Component {
   render() {
     return (
       <Row xs={1} md={3} lg={6} className="g-4 mx-2">
         {fantasy.map((book) => (
-<
+          <Col key={book.asin} className="d-flex">
+            <SingleBook img={book.img} />
+          </Col>
         ))}
       </Row>
     );
@@ -15,4 +18,3 @@ class AllTheBooks extends Component {
 }
 
 export default AllTheBooks;
-
