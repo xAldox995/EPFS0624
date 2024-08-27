caf81945054b09e4970e5234f6501a92b8218c9d
xAldox995 <valzanialdo@gmail.com>
2024-08-27 17:31:45 +0200

    commit

--- Mese-3/Week2/epibooks_hooks/src/components/AddComments.jsx
+++ Mese-3/Week2/epibooks_hooks/src/components/AddComments.jsx
@@ -1,119 +1,110 @@
-import { Component } from 'react'
-import { Button, Form } from 'react-bootstrap'
+import { useEffect, useState } from "react";
+import { Button, Form } from "react-bootstrap";
 
-class AddComment extends Component {
-  state = {
-    review: {
-      comment: '',
-      rate: '1',
-      elementId: this.props.asin, // questo è l'asin del libro selezionato,
-      // lo prendiamo direttamente dalle props che ci passa CommentArea e
-      // lo assegniamo come valore a elementId nello stato
+const AddComment = ({ asin, changeUpdateCommentsList }) => {
+  const [review, setReview] = useState({
+    comment: "",
+    rate: "1",
+    elementId: asin,
+  });
 
-      // ora this.props.asin inizialmente è stringa vuota!
-      // dovremmo mantenerlo aggiornato ogni volta che selezioniamo un nuovo libro
-    },
-  }
+  useEffect(() => {
+    setReview((prevReview) => ({
+      ...prevReview,
+      elementId: asin,
+    }));
+  }, [asin]);
+  // componentDidUpdate = (prevProps) => {
+  //   if (prevProps.asin !== this.props.asin) {
+  //     // ora manteniamo aggiornato elementId in this.state.rewiew
+  //     this.setState({
+  //       review: {
+  //         ...this.state.review,
+  //         elementId: this.props.asin,
+  //         // ora l'asin nello state è aggiornato con il valore ricevuto nelle props
+  //         // (valore che abbiamo ricevuto quando abbiamo cliccato su un nuovo libro)
+  //       },
+  //     })
+  //   }
+  // }
 
-  componentDidUpdate = (prevProps) => {
-    if (prevProps.asin !== this.props.asin) {
-      // ora manteniamo aggiornato elementId in this.state.rewiew
-      this.setState({
-        review: {
-          ...this.state.review,
-          elementId: this.props.asin,
-          // ora l'asin nello state è aggiornato con il valore ricevuto nelle props
-          // (valore che abbiamo ricevuto quando abbiamo cliccato su un nuovo libro)
-        },
-      })
-    }
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
+  const handleSubmit = (e) => {
+    e.preventDefault();
     // postiamo il nuovo commento
-    fetch('https://striveschool-api.herokuapp.com/api/comments/', {
-      method: 'POST',
+    fetch("https://striveschool-api.herokuapp.com/api/comments/", {
+      method: "POST",
       headers: {
         Authorization:
-          'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3NTU5MDQzYTU2ODAwMTU4ZWM0N2QiLCJpYXQiOjE3MjQzMzk2MDAsImV4cCI6MTcyNTU0OTIwMH0.3s4fBf_wAd6_WnhGb2s25J5AGcbrlGuBdE9CbFjQhPQ',
-        'Content-Type': 'application/json',
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3NTU5MDQzYTU2ODAwMTU4ZWM0N2QiLCJpYXQiOjE3MjQzMzk2MDAsImV4cCI6MTcyNTU0OTIwMH0.3s4fBf_wAd6_WnhGb2s25J5AGcbrlGuBdE9CbFjQhPQ",
+        "Content-Type": "application/json",
       },
       body: JSON.stringify(this.state.review),
     })
       .then((response) => {
         if (response.ok) {
-          alert('Commento salvato!')
+          alert("Commento salvato!");
           // quando il commento viene salvato con successo,
           // cambio lo stato di CommentArea
           //
-          this.props.changeUpdateCommentsList()
+          changeUpdateCommentsList();
           //
           this.setState({
             review: {
               // elementId: this.state.elementId,
-              ...this.state.review,
-              rate: '1',
-              comment: '',
+              ...review,
+              rate: "1",
+              comment: "",
             },
-          })
+          });
         } else {
-          alert('Commento NON salvato!')
-          throw new Error('Errore nel salvataggio')
+          alert("Commento NON salvato!");
+          throw new Error("Errore nel salvataggio");
         }
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
-  render() {
-    return (
-      <div className="mt-3">
-        <Form onSubmit={this.handleSubmit}>
-          <Form.Select
-            value={this.state.review.rate}
-            onChange={(e) => {
-              this.setState({
-                review: {
-                  ...this.state.review,
-                  rate: e.target.value,
-                },
+  return (
+    <div className="mt-3">
+      <Form onSubmit={handleSubmit}>
+        <Form.Select
+          value={review.rate}
+          onChange={(e) => 
+            setReview({
+              ...review,
+              rate : e.target.value
+            })
+          }
+        >
+          <option>1</option>
+          <option>2</option>
+          <option>3</option>
+          <option>4</option>
+          <option>5</option>
+        </Form.Select>
+        <Form.Group>
+          <Form.Control
+            placeholder="Scrivi la tua review"
+            value={review.comment}
+            onChange={(e) => 
+              setReview ({
+                ...review,
+                comment : e.target.value
               })
-            }}
-          >
-            <option>1</option>
-            <option>2</option>
-            <option>3</option>
-            <option>4</option>
-            <option>5</option>
-          </Form.Select>
-          <Form.Group>
-            <Form.Control
-              placeholder="Scrivi la tua review"
-              value={this.state.review.comment}
-              onChange={(e) => {
-                this.setState({
-                  review: {
-                    ...this.state.review, // spread operator
-                    // un modo in cui trascinare qui dentro tutto il contenuto
-                    // di un altro oggetto
-                    comment: e.target.value,
-                  },
-                })
-              }}
-            />
-          </Form.Group>
-          <Button variant="success" type="submit">
-            INVIA
-          </Button>
-        </Form>
-      </div>
-    )
-  }
-}
+            }
+          />
+        </Form.Group>
+        <Button variant="success" type="submit">
+          INVIA
+        </Button>
+      </Form>
+    </div>
+  );
+};
 
-export default AddComment
+export default AddComment;
 
 // const arr1 = ['uno', 'due', 'tre']
 
--- Mese-3/Week2/epibooks_hooks/src/components/CommentArea.jsx
+++ Mese-3/Week2/epibooks_hooks/src/components/CommentArea.jsx
@@ -1,81 +1,76 @@
-import { Component } from 'react'
-import CommentsList from './CommentsList'
-import AddComment from './AddComments'
+import { useEffect, useState } from "react";
+import CommentsList from "./CommentsList";
+import AddComment from "./AddComments";
 
-class CommentArea extends Component {
-  state = {
-    comments: [],
-    updateCommentsList: false,
-  }
+const CommentArea = ({ asin }) => {
+  const [comments, setComments] = useState([]);
 
-  changeUpdateCommentsList = () => {
-    this.setState({
-      updateCommentsList: !this.state.updateCommentsList,
-    })
-  }
+  const [updateCommentsList, setUpdateCommentsList] = useState(false);
 
-  componentDidMount = () => {
-    this.fetchComments()
-  }
+  useEffect(() => {
+    fetchComments();
+  }, [asin]);
 
-  // la fetch all'avvio va anche bene, ma quello che ci serve sarebbe
-  // richiamare la fetch ulteriormente ogni volta che cambia l'asin selezionato
+  useEffect(() => {
+    fetchComments();
+    setUpdateCommentsList(false);
+  }, [updateCommentsList]);
 
-  componentDidUpdate = (prevProps, prevState) => {
-    if (prevProps.asin !== this.props.asin) {
-      // abbiamo cliccato su un nuovo libro!
-      this.fetchComments()
-    }
+  // componentDidMount = () => {
+  //   this.fetchComments();
+  // };
 
-    if (prevState.updateCommentsList !== this.state.updateCommentsList) {
-      this.fetchComments()
-      // recupero nuovamente i commenti anche quando cambia la variabile di stato
-      // (updateCommentsList) che avevo creato per avvisarmi di quanto AddComment
-      // posta con successo una recensione
-    }
-  }
+  // // la fetch all'avvio va anche bene, ma quello che ci serve sarebbe
+  // // richiamare la fetch ulteriormente ogni volta che cambia l'asin selezionato
 
-  fetchComments = () => {
+  // componentDidUpdate = (prevProps, prevState) => {
+  //   if (prevProps.asin !== this.props.asin) {
+  //     // abbiamo cliccato su un nuovo libro!
+  //     this.fetchComments();
+  //   }
+
+  //   if (prevState.updateCommentsList !== this.state.updateCommentsList) {
+  //     this.fetchComments();
+  //     // recupero nuovamente i commenti anche quando cambia la variabile di stato
+  //     // (updateCommentsList) che avevo creato per avvisarmi di quanto AddComment
+  //     // posta con successo una recensione
+  //   }
+  // };
+
+  const fetchComments = () => {
     // fetch(`https://striveschool-api.herokuapp.com/api/comments/${this.props.asin}`)
-    fetch(
-      'https://striveschool-api.herokuapp.com/api/comments/' + this.props.asin,
-      {
-        headers: {
-          Authorization:
-            'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3NTU5MDQzYTU2ODAwMTU4ZWM0N2QiLCJpYXQiOjE3MjQzMzk2MDAsImV4cCI6MTcyNTU0OTIwMH0.3s4fBf_wAd6_WnhGb2s25J5AGcbrlGuBdE9CbFjQhPQ',
-        },
-      }
-    )
+    fetch("https://striveschool-api.herokuapp.com/api/comments/" + asin, {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NmM3NTU5MDQzYTU2ODAwMTU4ZWM0N2QiLCJpYXQiOjE3MjQzMzk2MDAsImV4cCI6MTcyNTU0OTIwMH0.3s4fBf_wAd6_WnhGb2s25J5AGcbrlGuBdE9CbFjQhPQ",
+      },
+    })
       .then((response) => {
         if (response.ok) {
-          return response.json()
+          return response.json();
         } else {
-          throw new Error('errore nel recupero delle recensioni')
+          throw new Error("errore nel recupero delle recensioni");
         }
       })
       .then((arrayOfComments) => {
-        console.log(arrayOfComments)
-        this.setState({
-          comments: arrayOfComments, // metto le recensioni da API nello stato
-        })
+        console.log(arrayOfComments);
+        setComments(arrayOfComments);
       })
       .catch((err) => {
-        console.log(err)
-      })
-  }
+        console.log(err);
+      });
+  };
 
-  render() {
-    return (
-      <div>
-        <CommentsList comments={this.state.comments} />
-        <AddComment
-          asin={this.props.asin}
-          changeUpdateCommentsList={this.changeUpdateCommentsList}
-        />
-        {/* prop drilling: prendo una prop da sopra, SingleBook, a sotto, AddComment */}
-      </div>
-    )
-  }
-}
+  return (
+    <div>
+      <CommentsList comments={comments} />
+      <AddComment
+        asin={asin}
+        changeUpdateCommentsList={() => setUpdateCommentsList(true)}
+      />
+      {/* prop drilling: prendo una prop da sopra, SingleBook, a sotto, AddComment */}
+    </div>
+  );
+};
 
-export default CommentArea
+export default CommentArea;
