1873b84e1865c36bbbcedfe540e07f461c4cbeea
xAldox995 <valzanialdo@gmail.com>
2024-08-29 17:00:22 +0200

    3 DO

--- Mese-3/Week2/epibooks_hooks/src/__test__/CommentArea.test.jsx
+++ Mese-3/Week2/epibooks_hooks/src/__test__/CommentArea.test.jsx
@@ -1,18 +1,19 @@
-import { render, screen } from "@testing-library/react"
-import CommentArea from "../components/CommentArea"
+import { render, screen } from "@testing-library/react";
+import CommentArea from "../components/CommentArea";
+import scifi from "../data/scifi.json" 
 
 // V - TO DO : VERIFICARE CHE COMMENT AREA SI MONTI CORRETTAMENTE
 
-describe('cooment list behavior', () => {
-    it("doesn't render any comment item initially", () => {
-        render(<CommentArea/>)
-        const allCommentItems = screen.queryAllByTestId('comment-element')
-        expect(allCommentItems).toHaveLength(0)
-    })
+describe("comment list behavior", () => {
+  it("doesn't render any comment item initially", () => {
+    render(<CommentArea />);
+    const allCommentItems = screen.queryAllByTestId("comment-element");
+    expect(allCommentItems).toHaveLength(0);
+  });
 
-    it("shoould have comment list when component is fully loads", async () => {
-        render(<CommentArea/>)
-        const allCommentItems = await screen.findAllByTestId('comment-element')
-        expect(allCommentItems.length).toBeGreaterThan(0)
-    })
-})
+  it("should have comment list when component is fully loads", async () => {
+    render(<CommentArea asin={scifi[0].asin}/>);
+    const allCommentItems = await screen.findAllByTestId("comment-element");
+    expect(allCommentItems.length).toBeGreaterThan(0);
+  });
+});
