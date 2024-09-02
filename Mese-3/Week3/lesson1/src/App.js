7ba873957cbbb3bab8a421199b358623f1bdce46
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-02 15:32:59 +0200

    create adds button

--- Mese-3/Week3/lesson1/src/App.js
+++ Mese-3/Week3/lesson1/src/App.js
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<MainSearch />} />
         <Route path="/:company" element={<CompanySearchResults />} />
+        <Route path="/favorites" element/>
       </Routes>
     </BrowserRouter>
   );
--- Mese-3/Week3/lesson1/src/components/Job.jsx
+++ Mese-3/Week3/lesson1/src/components/Job.jsx
@@ -1,20 +1,23 @@
-import { Row, Col } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Job = ({ data }) => (
   <Row
     className="mx-0 mt-3 p-3"
-    style={{ border: '1px solid #00000033', borderRadius: 4 }}
+    style={{ border: "1px solid #00000033", borderRadius: 4 }}
   >
     <Col xs={3}>
       <Link to={`/${data.company_name}`}>{data.company_name}</Link>
     </Col>
-    <Col xs={9}>
+    <Col xs={6}>
       <a href={data.url} target="_blank" rel="noreferrer">
         {data.title}
       </a>
     </Col>
+    <Col xs={3}>
+      <Button variant="outline-info">Adds to Favorites</Button>
+    </Col>
   </Row>
-)
+);
 
-export default Job
+export default Job;
