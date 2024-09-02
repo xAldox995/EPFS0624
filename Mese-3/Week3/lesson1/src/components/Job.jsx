fb5f376120fac4b393cc8e31d43c061dde5e2ef2
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-02 15:49:15 +0200

    connit

--- Mese-3/Week3/lesson1/src/components/Job.jsx
+++ Mese-3/Week3/lesson1/src/components/Job.jsx
@@ -1,23 +1,35 @@
 import { Row, Col, Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {useDispatch} from 'react-redux'
 
-const Job = ({ data }) => (
-  <Row
-    className="mx-0 mt-3 p-3"
-    style={{ border: "1px solid #00000033", borderRadius: 4 }}
-  >
-    <Col xs={3}>
-      <Link to={`/${data.company_name}`}>{data.company_name}</Link>
-    </Col>
-    <Col xs={6}>
-      <a href={data.url} target="_blank" rel="noreferrer">
-        {data.title}
-      </a>
-    </Col>
-    <Col xs={3}>
-      <Button variant="outline-info">Adds to Favorites</Button>
-    </Col>
-  </Row>
-);
+const Job = ({ data }) => {
 
+  const dispatch = useDispatch()
+  return (
+    <Row
+      className="mx-0 mt-3 p-3"
+      style={{ border: "1px solid #00000033", borderRadius: 4 }}
+    >
+      <Col xs={3}>
+        <Link to={`/${data.company_name}`}>{data.company_name}</Link>
+      </Col>
+      <Col xs={6}>
+        <a href={data.url} target="_blank" rel="noreferrer">
+          {data.title}
+        </a>
+      </Col>
+      <Col xs={3}>
+        <Button variant="outline-info"
+        onClick={() => {
+          console.log('TASTO FUNZIONA')
+          dispatch({
+            type : 'ADD_JOB',
+            payload : data
+          })
+        }}>
+          Adds to Favorites</Button>
+      </Col>
+    </Row>
+  );
+};
 export default Job;
--- Mese-3/Week3/lesson1/src/redux/reducer/index.js
+++ Mese-3/Week3/lesson1/src/redux/reducer/index.js
@@ -1,11 +1,20 @@
 const initialState = {
-    favorite: {
+    favorites: {
         jobOffers: []
     }
 }
 
 const mainReducer = (state = initialState, action) => {
     switch (action.type) {
+        case 'ADD_JOB': return {
+            ...state,
+            favorites : {
+                ... state.favorites,
+                jobOffers:[...state.favorites.jobOffers, action.payload],
+            },
+        }
+
+
 
         default: {
             return state
