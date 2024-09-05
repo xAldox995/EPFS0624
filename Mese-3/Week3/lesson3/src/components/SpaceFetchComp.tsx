dc9e6f6dd753ae2b52287ae3f1cce295f422e724
valzanialdo@gmail.com <LA-TUA-EMAIL>
2024-09-05 16:42:08 +0200

    commit

--- Mese-3/Week3/lesson3/src/components/SpaceFetchComp.tsx
+++ Mese-3/Week3/lesson3/src/components/SpaceFetchComp.tsx
@@ -1,5 +1,7 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Article from "./utilities/Artticle";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const SpaceFetchComp = () => {
   const [articles, setArtcles] = useState<Article[]>([]);
@@ -8,22 +10,51 @@ const SpaceFetchComp = () => {
       .then((response) => {
         if (response.ok) {
           return response.json();
-        } else { 
-            throw new Error('Errore nel recupero dati')
+        } else {
+          throw new Error("Errore nel recupero dati");
         }
       })
       .then((articlesData) => {
-        console.log(articlesData)
-        setArtcles(articlesData)
+        console.log(articlesData);
+        setArtcles(articlesData);
       })
       .catch((err) => {
         console.log(err);
       });
   };
 
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const navigate = useNavigate()
+
   return (
-    <h1>CIAO SONO LA COMP DOVE FACCIO LA FETCH</h1>
-  )
+    <Container>
+      <h1>SPACE NEWS ARTICLES</h1>
+      <Row xs={1} md={2} lg={5} className="g-2">
+        {articles.map((article) => {
+          return (
+            <Col key={article.id}>
+              <Card>
+                <Card.Img variant="top" src={article.image_url} />
+                <Card.Body>
+                  <Card.Title>{article.title}</Card.Title>
+                  <Card.Text>{article.summary}</Card.Text>
+                  <Button
+                    variant="primary"
+                    onClick={()=> navigate('/article/'+ article.id)}
+                  >
+                    Read More
+                  </Button>
+                </Card.Body>
+              </Card>
+            </Col>
+          );
+        })}
+      </Row>
+    </Container>
+  );
 };
 
-export default SpaceFetchComp
+export default SpaceFetchComp;
